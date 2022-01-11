8fbda9042a8d6cd54a475e32151a54c6880681b3
TENCENT\v_ruofanwu <wrf_456@163.com>
2022-01-11 22:03:20 +0800

    添加GameEntry类

---
+++ UnityGameFramework-master/Scripts/GameEntry.cs
@@ -0,0 +1,244 @@
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace Tutorial
+{
+    /// <summary>
+    /// 游戏入口
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+        /// <summary>
+        /// 游戏基础组件
+        /// </summary>
+        public static BaseComponent Base
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取配置组件
+        /// </summary>
+        public static ConfigComponent Config
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取数据节点组件
+        /// </summary>
+        public static DataNodeComponent DataNode
+        {
+            get;
+            private set;
+        }
+        
+        /// <summary>
+        /// 获取数据表组件
+        /// </summary>
+        public static DataTableComponent DataTable
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取调试组件
+        /// </summary>
+        public static DebuggerComponent Debugger
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取下载组件。
+        /// </summary>
+        public static DownloadComponent Download
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取实体组件。
+        /// </summary>
+        public static EntityComponent Entity
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取事件组件。
+        /// </summary>
+        public static EventComponent Event
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取文件系统组件。
+        /// </summary>
+        public static FileSystemComponent FileSystem
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取有限状态机组件。
+        /// </summary>
+        public static FsmComponent Fsm
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取本地化组件。
+        /// </summary>
+        public static LocalizationComponent Localization
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取网络组件。
+        /// </summary>
+        public static NetworkComponent Network
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取对象池组件。
+        /// </summary>
+        public static ObjectPoolComponent ObjectPool
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取流程组件。
+        /// </summary>
+        public static ProcedureComponent Procedure
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取资源组件。
+        /// </summary>
+        public static ResourceComponent Resource
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取场景组件。
+        /// </summary>
+        public static SceneComponent Scene
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取配置组件。
+        /// </summary>
+        public static SettingComponent Setting
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取声音组件。
+        /// </summary>
+        public static SoundComponent Sound
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取界面组件。
+        /// </summary>
+        public static UIComponent UI
+        {
+            get;
+            private set;
+        }
+
+        /// <summary>
+        /// 获取网络组件。
+        /// </summary>
+        public static WebRequestComponent WebRequest
+        {
+            get;
+            private set;
+        }
+
+        private static void InitBuiltinComponents()
+        {
+            Base = UnityGameFramework.Runtime.GameEntry.GetComponent<BaseComponent>();
+            Config = UnityGameFramework.Runtime.GameEntry.GetComponent<ConfigComponent>();
+            DataNode = UnityGameFramework.Runtime.GameEntry.GetComponent<DataNodeComponent>();
+            DataTable = UnityGameFramework.Runtime.GameEntry.GetComponent<DataTableComponent>();
+            Debugger = UnityGameFramework.Runtime.GameEntry.GetComponent<DebuggerComponent>();
+            Download = UnityGameFramework.Runtime.GameEntry.GetComponent<DownloadComponent>();
+            Entity = UnityGameFramework.Runtime.GameEntry.GetComponent<EntityComponent>();
+            Event = UnityGameFramework.Runtime.GameEntry.GetComponent<EventComponent>();
+            FileSystem = UnityGameFramework.Runtime.GameEntry.GetComponent<FileSystemComponent>();
+            Fsm = UnityGameFramework.Runtime.GameEntry.GetComponent<FsmComponent>();
+            Localization = UnityGameFramework.Runtime.GameEntry.GetComponent<LocalizationComponent>();
+            Network = UnityGameFramework.Runtime.GameEntry.GetComponent<NetworkComponent>();
+            ObjectPool = UnityGameFramework.Runtime.GameEntry.GetComponent<ObjectPoolComponent>();
+            Procedure = UnityGameFramework.Runtime.GameEntry.GetComponent<ProcedureComponent>();
+            Resource = UnityGameFramework.Runtime.GameEntry.GetComponent<ResourceComponent>();
+            Scene = UnityGameFramework.Runtime.GameEntry.GetComponent<SceneComponent>();
+            Setting = UnityGameFramework.Runtime.GameEntry.GetComponent<SettingComponent>();
+            Sound = UnityGameFramework.Runtime.GameEntry.GetComponent<SoundComponent>();
+            UI = UnityGameFramework.Runtime.GameEntry.GetComponent<UIComponent>();
+            WebRequest = UnityGameFramework.Runtime.GameEntry.GetComponent<WebRequestComponent>();
+        }
+
+        private static void InitCustomComponents()
+        {
+            // 将来在这里注册自定义的组件
+        }
+
+        private static void InitCustomDebuggers()
+        {
+            // 将来在这里注册自定义的调试器
+        }
+    }
+
+
+    /// <summary>
+    /// 游戏入口
+    /// </summary>
+    public partial class GameEntry : MonoBehaviour
+    {
+        private void Start()
+        {
+            // 初始化内置组件
+            InitBuiltinComponents();
+
+            // 初始化自定义组件
+            InitCustomComponents();
+
+            // 初始化自定义调试器
+            InitCustomDebuggers();
+        }
+    }
+}
