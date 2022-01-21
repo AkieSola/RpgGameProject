f38a801aca67dbde5092e351369854a11b4e7fb5
ruofanwu <wrf_456@163.com>
2022-01-21 18:24:52 +0800

    actor实体基类

---
+++ GameMain/Scripts/Entity/EntityData/ActorData.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RPGGame
+{
+    public abstract class ActorData : EntityData
+    {
+        [SerializeField]
+        private int m_HP = 0;
+        [SerializeField]
+        private int m_SP = 0;
+
+        [SerializeField]
+        private CampType m_Camp = CampType.Unknown;
+        public ActorData(int entityId, int typeId, CampType camp) : base(entityId, typeId)
+        {
+            m_Camp = camp;
+            m_HP = 0;
+            m_SP = 0;
+        }
+
+        /// <summary>
+        /// 生命值
+        /// </summary>
+        public int HP { get => m_HP; set => m_HP = Math.Max(0, value); }
+
+        /// <summary>
+        /// 最大生命
+        /// </summary>
+        public abstract int MaxHP
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 行动值
+        /// </summary>
+        public int SP { get => m_SP; set => m_SP = value; }
+
+        /// <summary>
+        /// 最大行动值
+        /// </summary>
+        public abstract int MaxSp
+        {
+            get;
+        }
+
+        /// <summary>
+        /// 生命百分比
+        /// </summary>
+        public float HPRatio
+        {
+            get
+            {
+                return MaxHP > 0 ? (float)HP / MaxHP : 0f;
+            }
+        }
+
+        /// <summary>
+        /// 阵营
+        /// </summary>
+        public CampType Camp { get => m_Camp;}
+    }
+}
---
+++ GameMain/Scripts/Entity/EntityLogic/Actor.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityGameFramework.Runtime;
+
+namespace RPGGame
+{
+    public class Actor : Entity
+    {
+        [SerializeField]
+        private ActorData m_ActorData = null;
+
+        public bool IsDead
+        {
+            get
+            {
+                return m_ActorData.HP <= 0;
+            }
+        }
+
+        public void ApplyDamage(Entity attacker, int damageHP)
+        {
+            m_ActorData.HP -= damageHP;
+            if (m_ActorData.HP <= 0)
+            {
+                OnDead(attacker);
+            }
+        }
+
+        protected override void OnInit(object userData)
+        {
+            m_ActorData = userData as ActorData;
+            if (m_ActorData == null)
+            {
+                Log.Error("Actor data is invalid.");
+            }
+            base.OnInit(userData);
+        }
+
+        protected override void OnShow(object userData)
+        {
+            base.OnShow(userData);
+        }
+
+        protected virtual void OnDead(Entity attacker)
+        {
+
+        }
+    }
+}
